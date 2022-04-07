3438dd51fe4c8217bbf7cb14baed977c3ad8f022
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-04-07 16:02:17 -0400

    commit tree af1bf1a6925ab8aa454ca0a5d66cdb28387180e3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  
+  const linksElements = links.map((link) => {
+    return <a href ="#home" key= {link}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linksElements}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techList = technologies.map((technology) => {
+    console.log(technologies.name)
+    return <span>{technology}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techList}</div>
     </div>
   );
 }
